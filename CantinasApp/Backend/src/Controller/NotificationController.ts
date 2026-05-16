7dc398ae7473ead21a5f3106fa22ea30b0548e66
JoaoGondar <128156319+JoaoGondar@users.noreply.github.com>
2026-05-16 11:54:44 +0100

    Fix import case for NotificationService

--- CantinasApp/Backend/src/Controller/NotificationController.ts
+++ CantinasApp/Backend/src/Controller/NotificationController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { NotificationService } from "../Service/notificationService";
+import { NotificationService } from "../Service/NotificationService";
 
 export class NotificationController {
     static async create(req: Request, res: Response) {
